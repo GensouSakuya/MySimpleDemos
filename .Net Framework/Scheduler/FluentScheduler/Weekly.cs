7a8507e762a0f52874a3fb0d1b99eb774a192c82
GensouSakuya <22719048+GensouSakuya@users.noreply.github.com>
2020-09-09 17:09:34 +0800

    add fluentscheduler

---
+++ .Net Framework/Scheduler/FluentScheduler/Weekly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentScheduler
+{
+    internal class Weekly
+    {
+        public static void Interval(int n, DayOfWeek week)
+        {
+            new Registry().Schedule<Job>().ToRunEvery(2).Weeks().On(DayOfWeek.Monday);
+        }
+    }
+}
